ca7d30fea1be9f9708af5136b631f1ef67f0147b
Stefan Rothe <stefan.rothe@gymkirchenfeld.ch>
2024-12-14 08:28:02 +0100

    Improved build.sh, compile dependencies too.

--- build.sh
+++ build.sh
@@ -1,5 +1,18 @@
 #!/bin/sh
 
+# abort on error
+set -e
+
 # verify pwd
 BASE_DIR=$(cd $(dirname "$0"); pwd)
+
+PARENT_DIR=$(dirname "${BASE_DIR}")
+
+cd ${PARENT_DIR}/ch.kinet.datalib
+mvn clean install
+
+cd ${PARENT_DIR}/ch.kinet.pdflib
+mvn clean install
+
+cd ${BASE_DIR}
 mvn clean compile install
